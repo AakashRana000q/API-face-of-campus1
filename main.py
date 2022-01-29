3b1dec0ac5fb354ca51b47ccb875157f3b46a084
AakashRana000q <aakashrana000q@gmail.com>
2022-01-29 16:38:51 +0530

    make it better

--- main.py
+++ main.py
@@ -20,19 +20,21 @@ def predict():
     places=[]
     branches=[]
     images=[]
+    year=[]
     if(name!=''):
         for (i,namee) in enumerate(user_data['name']):
             if namee==name:
                 names.append(namee)
                 places.append(user_data['place'][i])
                 branches.append(user_data['branch'][i])
+                year.append(user_data['year'][i])
                 img=user_data['image'][i]
                 rawBytes = io.BytesIO()
                 img.save(rawBytes, "JPEG")
                 rawBytes.seek(0)
                 img_base64 = base64.b64encode(rawBytes.read())
                 images.append(str(img_base64))
-        result={'name':names,'place':places,'branch':branches,'image':images}
+        result={'name':names,'place':places,'branch':branches,'image':images,'year':year}
         return jsonify(result)
     elif(place!=''):
         for (i,placee) in enumerate(user_data['place']):
@@ -40,13 +42,14 @@ def predict():
                 places.append(placee)
                 names.append(user_data['name'][i])
                 branches.append(user_data['branch'][i])
+                year.append(user_data['year'][i])
                 img = user_data['image'][i]
                 rawBytes = io.BytesIO()
                 img.save(rawBytes, "JPEG")
                 rawBytes.seek(0)
                 img_base64 = base64.b64encode(rawBytes.read())
                 images.append(str(img_base64))
-        result={'name':names,'place':places,'branch':branches,'image':images}
+        result={'name':names,'place':places,'branch':branches,'image':images,'year':year}
         return jsonify(result)
     elif (branch != ''):
         for (i, branchee) in enumerate(user_data['branch']):
@@ -54,13 +57,14 @@ def predict():
                 branches.append(branchee)
                 names.append(user_data['name'][i])
                 places.append(user_data['place'][i])
+                year.append(user_data['year'][i])
                 img = user_data['image'][i]
                 rawBytes = io.BytesIO()
                 img.save(rawBytes, "JPEG")
                 rawBytes.seek(0)
                 img_base64 = base64.b64encode(rawBytes.read())
                 images.append(str(img_base64))
-        result = {'name': names, 'place': places, 'branch': branches,'image':images}
+        result = {'name': names, 'place': places, 'branch': branches,'image':images,'year':year}
         return jsonify(result)
 
 
